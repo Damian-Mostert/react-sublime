e9d27c84900bde60e06c4626e523419e84914f86
Damian Mostert <damianmostert86@gmail.com>
2024-05-08 21:17:27 +0200

    x

--- bin.js
+++ bin.js
@@ -1,38 +1,3 @@
 #!/usr/bin/env node
-const prompt = async (msg) => {
-  console.log(msg);
-  return await new Promise((Resolve) => {
-    process.stdin.on("data", (message) => {
-      Resolve(message.toString());
-    });
-  });
-};
 
-//GLOBALS
-const main = async () => {
-  fs = require("fs");
-  __dirname = process.cwd();
-
-  __config = {};
-  try {
-    __config = require(__dirname + "/sublime.config.json");
-  } catch (e) {
-    const inputDirectory = await prompt("Please enter your input directory");
-    const outputDirectory = await prompt("Please enter your output directory");
-    __config = { inputDirectory, outputDirectory };
-    fs.writeFileSync(
-      __dirname + "/sublime.config.json",
-      JSON.stringify(__config, 4, null)
-    );
-    console.log("sublime config successfully made.");
-    require("./lib/init");
-  }
-
-  switch (process.argv[2]) {
-    case "init":
-      return require("./lib/init");
-    case "build":
-      return require("./lib/build");
-  }
-};
-main();
+require("./lib/main");
